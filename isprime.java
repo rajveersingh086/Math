bbaee19ee43ead0a03d962e33cfa0429e56ea36c
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2025-03-29 23:35:26 +0530

    Add files via upload

---
+++ isprime.java
@@ -0,0 +1,38 @@
+package mathematics;
+import java.util.*;
+     // TIME COMPLEXITY : O(N^1/2)
+
+public class isprime {
+    public boolean isPrime(int N) {
+        if (N <= 1) {
+            return false;
+        }
+        if (N <= 3) {
+            return true;
+        }
+        if (N % 2 == 0 || N % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= N; i += 6) {
+            if (N % i == 0 || N % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        isprime solution = new isprime();
+        if (solution.isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
+
+
