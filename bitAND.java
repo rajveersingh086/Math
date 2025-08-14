5a5e10d03bfa0eeabf829082c21ff25a1923889b
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2025-08-14 23:10:41 +0530

    Add files via upload

---
+++ bitAND.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class bitAND {
+
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+}
